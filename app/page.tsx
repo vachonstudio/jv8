b5db8bdfbd77ed798c9889ca5ab4050c9c4f1438
vachonstudio <65257651+vachonstudio@users.noreply.github.com>
2025-06-29 17:12:10 -0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { AppContent } from '../components/AppContent'
+import { ThemeProvider } from '../contexts/ThemeContext'
+import { AuthProvider } from '../contexts/AuthContext'
+
+export default function HomePage() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <AppContent />
+      </AuthProvider>
+    </ThemeProvider>
+  )
+}
